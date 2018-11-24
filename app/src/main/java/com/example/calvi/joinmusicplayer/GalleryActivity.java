8e97184468f8c08aa60bbf17e38de194470e7dbf
etirtana <erictirtana@yahoo.com>
2018-11-24 10:36:35 +0700

    Update 24/11/2018
    Done:
    - Stream From Drive
    - RecyclerView

---
+++ app/src/main/java/com/example/calvi/joinmusicplayer/GalleryActivity.java
@@ -0,0 +1,70 @@
+package com.example.calvi.joinmusicplayer;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private static final String TAG = "GalleryActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+        Log.d(TAG, "onCreate: started.");
+
+        getIncomingIntent();
+
+
+        mediaPlayer = new MediaPlayer();
+        String file_path = "https://docs.google.com/uc?export=download&id=1byjVHp_6hL_g_9tNhb6yJC6NFpkMZMGs";
+
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        try {
+            mediaPlayer.setDataSource(file_path);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getIncomingIntent(){
+        Log.d(TAG, "getIncomingIntent: checking for incoming intents.");
+
+        if(getIntent().hasExtra("image_url") && getIntent().hasExtra("image_name")){
+            Log.d(TAG, "getIncomingIntent: found intent extras.");
+
+            String imageUrl = getIntent().getStringExtra("image_url");
+            String imageName = getIntent().getStringExtra("image_name");
+
+            setImage(imageUrl, imageName);
+        }
+    }
+
+
+    private void setImage(String imageUrl, String imageName){
+        Log.d(TAG, "setImage: setting te image and name to widgets.");
+
+        TextView name = findViewById(R.id.image_description);
+        name.setText(imageName);
+
+        ImageView image = findViewById(R.id.image);
+        Glide.with(this)
+                .asBitmap()
+                .load(imageUrl)
+                .into(image);
+    }
+}
--- app/src/main/java/com/example/calvi/joinmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/calvi/joinmusicplayer/MainActivity.java
@@ -1,18 +1,22 @@
 package com.example.calvi.joinmusicplayer;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText username;
     private EditText password;
     private Button loginButton;
+    private Button signUpButton;
 
 
     @Override
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         username = (EditText)findViewById(R.id.editTextUsername);
         password = (EditText)findViewById(R.id.editTextPassword);
         loginButton = (Button)findViewById(R.id.button);
+        signUpButton = (Button)findViewById(R.id.button2);
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,15 +37,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openSignUpActivity();
+            }
+        });
+
 
     }
 
+
+    private void openSignUpActivity() {
+
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
 
     private void LoginValidation(String inputUsername, String inputPassword) {
 
         if (inputUsername.equals("admin") && inputPassword.equals("admin")) {
 
-            Intent intent = new Intent(this, HomeActivity.class);
+            Intent intent = new Intent(this, MusicListActivity.class);
             startActivity(intent);
         }
         else {
