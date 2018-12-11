bfee90aff7c1db5f76f9fa31649bd8805d61da3f
Calvin Kamtoso <calvinkmts@gmail.com>
2018-12-12 03:18:00 +0700

    Adding Login

--- app/src/main/java/com/example/calvi/joinmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/calvi/joinmusicplayer/MainActivity.java
@@ -4,12 +4,26 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,8 +64,68 @@ public class MainActivity extends AppCompatActivity {
 
     private void Login() {
 
+        final String nrp = mNrpEditText.getText().toString();
+        final String password = mPasswordEditText.getText().toString();
 
+        if (TextUtils.isEmpty(nrp)) {
+            mNrpEditText.setError("Please enter your nrp");
+            mNrpEditText.requestFocus();
+            return;
+        }
 
+        if (TextUtils.isEmpty(password)) {
+            mPasswordEditText.setError("Please enter your password");
+            mPasswordEditText.requestFocus();
+            return;
+        }
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URLs.URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+
+                            JSONObject object = new JSONObject(response);
+
+                            if (!object.getBoolean("error")) {
+                                Toast.makeText(getApplicationContext(), object.getString("message"),
+                                        Toast.LENGTH_SHORT).show();
+
+                                JSONObject userJson = object.getJSONObject("user");
+
+                                User user = new User(
+                                        userJson.getInt("id"),
+                                        userJson.getString("nrp"),
+                                        userJson.getString("nama"),
+                                        userJson.getString("email")
+                                );
+
+                                finish();
+                                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                            } else {
+                                Toast.makeText(getApplicationContext(), object.getString("message"), Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+        }) {
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("nrp", nrp);
+                params.put("password", password);
+                return params;
+            }
+        };
+        VolleySingleton.getInstance(this).addToRequestQueue(stringRequest);
     }
 
 
--- app/src/main/java/com/example/calvi/joinmusicplayer/URLs.java
+++ app/src/main/java/com/example/calvi/joinmusicplayer/URLs.java
@@ -2,7 +2,7 @@ package com.example.calvi.joinmusicplayer;
 
 public class URLs {
 
-    private static final String ROOT_URL = "http://192.168.100.4/joinmusic/Api.php?apicall=";
+    private static final String ROOT_URL = "https://joinmusic.kamtoso.com/api.php?apicall=";
 
     public static final String URL_REGISTER = ROOT_URL + "signup";
     public static final String URL_LOGIN = ROOT_URL + "login";
